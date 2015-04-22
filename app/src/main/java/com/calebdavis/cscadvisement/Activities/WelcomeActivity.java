6705bb3bda14754f64ff5351d7c7993e6171a02c
w741357 <caleb.e.davis@eagles.usm>
2015-04-22 01:49:54 -0500

    refactored

--- app/src/main/java/com/calebdavis/cscadvisement/Activities/WelcomeActivity.java
+++ app/src/main/java/com/calebdavis/cscadvisement/Activities/WelcomeActivity.java
@@ -1,54 +1,58 @@
 package com.calebdavis.cscadvisement.Activities;
 
+/**
+ * Created by Caleb Davis on 4/21/15.
+ */
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.calebdavis.cscadvisement.R;
-import com.parse.ParseAnonymousUtils;
 import com.parse.ParseUser;
 
-public class MainActivity extends Activity {
+public class WelcomeActivity extends Activity {
+
+    // Declare Variable
+    Button logout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Get the view from singleitemview.xml
+        setContentView(R.layout.welcome);
 
-        // Determine whether the current user is an anonymous user
-        if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
-            // If user is anonymous, send the user to LoginSignupActivity.class
+        // Retrieve current user from Parse.com
+        ParseUser currentUser = ParseUser.getCurrentUser();
 
-            Intent intent = new Intent(this, LoginSignupActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            // If current user is NOT anonymous user
-            // Get current user data from Parse.com
-            ParseUser currentUser = ParseUser.getCurrentUser();
-            if (currentUser != null) {
-                // Send logged in users to Welcome.class
-                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
-                startActivity(intent);
-                finish();
-            } else {
-                // Send user to LoginSignupActivity.class
-                Intent intent = new Intent(MainActivity.this,
-                        LoginSignupActivity.class);
-                startActivity(intent);
+        // Convert currentUser into String
+        String struser = currentUser.getUsername().toString();
+
+        // Locate TextView in welcome.xml
+        TextView txtuser = (TextView) findViewById(R.id.txtuser);
+
+        // Set the currentUser String into TextView
+        txtuser.setText("You are logged in as " + struser);
+
+        // Locate Button in welcome.xml
+        logout = (Button) findViewById(R.id.logout);
+
+        // Logout Button Click Listener
+        logout.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Logout current user
+                ParseUser.logOut();
                 finish();
             }
-        }
-
-
-
-
+        });
     }
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -84,7 +88,4 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
-
-
