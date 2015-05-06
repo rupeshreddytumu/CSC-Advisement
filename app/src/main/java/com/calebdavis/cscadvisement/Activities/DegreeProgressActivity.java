7649b3abd78c402293460431e3447ef2d85311b6
w741357 <caleb.e.davis@eagles.usm>
2015-05-06 17:24:01 -0500

    Changed DegreePlan to use different adapter

--- app/src/main/java/com/calebdavis/cscadvisement/Activities/DegreeProgressActivity.java
+++ app/src/main/java/com/calebdavis/cscadvisement/Activities/DegreeProgressActivity.java
@@ -2,69 +2,51 @@ package com.calebdavis.cscadvisement.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
-import com.calebdavis.cscadvisement.Services.CoursesDbAdapter;
 import com.calebdavis.cscadvisement.R;
 import com.calebdavis.cscadvisement.SQLiteTableClasses.StudentCourse;
+import com.calebdavis.cscadvisement.Services.CoursesDbAdapter;
+import com.calebdavis.cscadvisement.Services.MyCustomBaseAdapter;
 import com.parse.ParseUser;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by Caleb Davis on 4/27/15.
  */
 public class DegreeProgressActivity extends Activity {
 
-    // Database Helper
     private CoursesDbAdapter dbHelper;
-    private SimpleCursorAdapter dataAdapter;
-    String user_id;
-    long user_row_id;
-
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.courses);
-
         dbHelper = new CoursesDbAdapter(this);
         dbHelper.open();
 
+        ArrayList<StudentCourse> searchResults = GetSearchResults();
 
-        //Clean all data
-        //dbHelper.deleteAllStudentCourses();
-        //dbHelper.deleteAllCompletedCourses();
-        //Add some data
+        final ListView lv1 = (ListView) findViewById(R.id.coursesListView);
+        lv1.setAdapter(new MyCustomBaseAdapter(this, searchResults));
 
+
+    }
+
+    private ArrayList<StudentCourse> GetSearchResults(){
+        ArrayList<StudentCourse> results = new ArrayList<StudentCourse>();
 
         ParseUser currentUser = ParseUser.getCurrentUser();
         String student_id = currentUser.getUsername().toString();
-        this.user_id = student_id;
-        this.user_row_id = dbHelper.createStudent(student_id);
-        try{
-            List<StudentCourse> test_list = dbHelper.testCoursesForSpecificStudent(user_id);
-            if ( test_list.size() == 0){
-                dbHelper.insertStudentCourses(user_id);
 
-            }
+        results = (ArrayList<StudentCourse>) dbHelper.testCoursesForSpecificStudent(student_id);
+        Collections.sort(results);
 
-        }catch(Exception e){
-
-        }
-
-
-
-        //Generate ListView from SQLite Database
-        displayListView();
+        return results;
     }
 
     @Override
@@ -93,6 +75,13 @@ public class DegreeProgressActivity extends Activity {
             finish();
         }
 
+        if (id == R.id.courses){
+            // show courses taken
+            Intent intent = new Intent(this, DegreeProgressActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
 
         if (id == R.id.get_advised){
             // show courses taken
@@ -105,87 +94,7 @@ public class DegreeProgressActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void displayListView() {
-        List<StudentCourse> test = dbHelper.testCoursesForSpecificStudent(user_id);
-        Cursor cursor = dbHelper.fetchAllStudentCourses(user_id);
-        List<StudentCourse> coursesNotTaken = dbHelper.getAllCoursesNotTakenByStudent(user_id, "false");
-
-        String student_id_test = cursor.getString(cursor.getColumnIndexOrThrow("student_id"));
-        String course_id_test = cursor.getString(cursor.getColumnIndexOrThrow("course_id"));
-        final String status_test = cursor.getString(cursor.getColumnIndexOrThrow("status"));
-
-        // The desired columns to be bound
-        String[] columns = new String[] {
-                CoursesDbAdapter.KEY_STUDENT_ID,
-                CoursesDbAdapter.KEY_COURSE_ID,
-                CoursesDbAdapter.KEY_STATUS,
-        };
-
-        // the XML defined views which the data will be bound to
-        int[] to = new int[] {
-                R.id.sid,
-                R.id.cid,
-
-        };
-
-        // create the adapter using the cursor pointing to the desired data
-        //as well as the layout information
-        dataAdapter = new SimpleCursorAdapter(
-                this, R.layout.courses_info,
-                cursor,
-                columns,
-                to,
-                0);
-
-        ListView listView = (ListView) findViewById(R.id.coursesListView);
-        // Assign adapter to ListView
-        listView.setAdapter(dataAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> listView, View view,
-                                    int position, long id) {
-                // Get the cursor, positioned to the corresponding row in the result set
-                Cursor cursor = (Cursor) listView.getItemAtPosition(position);
-
-
-
-                    // Get the state's capital from this row in the database.
-                    String course_id =
-                            cursor.getString(cursor.getColumnIndexOrThrow("course_id"));
-                    String status =
-                            cursor.getString(cursor.getColumnIndexOrThrow("status"));
-                    long row_id = cursor.getLong(cursor.getColumnIndex("_id"));
-
-                    String newStatus = status;
-
-                    if (newStatus.equals("false")){
-
-                        newStatus = "true";
-                        dbHelper.updateItem(row_id, newStatus);
-                        //statusTextView.setText(newStatus);
-
-
-                    }
-                    else if (newStatus.equals("true")){
-
-                        newStatus = "false";
-                        dbHelper.updateItem(row_id, newStatus)  ;
-                        //dbHelper.updateItem(course_id, newStatus, user_id);
-                        //statusTextView.setText(newStatus);
-
-                    }
-
-                }
-
-
-        });
-
-    }
-
-
 }
-
 
 
 
